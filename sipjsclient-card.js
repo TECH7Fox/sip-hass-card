65e47200fbedc3be5fa22b41f0722171e4a1401c
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-10-10 21:44:41 +0200

    test

--- sipjsclient-card.js
+++ sipjsclient-card.js
@@ -19052,13 +19052,17 @@ class ContentCardExample extends HTMLElement {
         for(var client in this.config.clients) {
             console.log(client);
             if (deviceID == client) {
-                aor = this.config.clients[client].aor;
-                authorizationUsername = this.config.clients[client].username;
-                authorizationPassword = this.config.clients[client].password;
+                aor = client.aor;
+                authorizationUsername = client.username;
+                authorizationPassword = client.password;
                 //exit loop
             }
             // throw new Error('No settings for this deviceID');
         };
+
+        console.log(aor);
+        console.log(authorizationUsername);
+        console.log(authorizationPassword);
 
         //switch (deviceID) {
         //   case 'ae25c0fa-fb9777c5':
