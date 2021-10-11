03c6130698496811b6a5e75cd6d52c5baa57bf6f
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-10-11 21:03:52 +0200

    force update

--- sipjsclient-card.js
+++ sipjsclient-card.js
@@ -19039,7 +19039,7 @@ class ContentCardExample extends HTMLElement {
       if (!this.content) {
         this.innerHTML = `<ha-card header="` + (this.config.title ? this.config.title : "") + `"><div class="card-content"></div></ha-card>`;
         this.content = this.querySelector('div');
-        this.content.innerHTML = `<audio id="remoteAudio" style="display:none"></audio><h2 style="text-align: center" id="name">Name</h2><span style="float:left" id="state">State</span><span style="float:right" id="time">time</span><br><hr><button id="answer">Answer</button><button id="hangup">Hangup</button>`;
+        this.content.innerHTML = `<audio id="remoteAudio" style="display:none"></audio><h2 style="text-align: center" id="name">Name10</h2><span style="float:left" id="state">State</span><span style="float:right" id="time">time</span><br><hr><button id="answer">Answer</button><button id="hangup">Hangup</button>`;
 
         console.log(this.config);
         const server = this.config.server;
