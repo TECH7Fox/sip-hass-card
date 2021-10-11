bf9b6bf0535b5432682ab11a8dd7760fbaf2fca7
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-10-11 22:53:04 +0200

    test icon

--- sipjsclient-card.js
+++ sipjsclient-card.js
@@ -19039,7 +19039,8 @@ class SIPjsClientCard extends HTMLElement {
       if (!this.content) {
         this.innerHTML = `<ha-card header="` + (this.config.title ? this.config.title : "") + `"><div class="card-content"></div></ha-card>`;
         this.content = this.querySelector('div');
-        this.content.innerHTML = `<audio id="remoteAudio" style="display:none"></audio><h2 style="text-align: center" id="name">Idle</h2><span style="float:left" id="state">State</span><span style="float:right" id="time">time</span><br><hr><button id="answer">Answer</button><button id="hangup">Hangup</button>`;
+        this.content.innerHTML = `<audio id="remoteAudio" style="display:none"></audio><h2 style="text-align: center" id="name">Idle</h2><span style="float:left" id="state">State</span><span style="float:right" id="time">time</span><br><hr><button id="answer"><ha-svg-icon><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="limegreen" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z"></path>
+        </svg></ha-svg-icon></button><button id="hangup">Hangup</button>`;
 
         console.log(this.config);
         const server = this.config.server;
